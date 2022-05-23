141b664711dff69fe43eced86b64d653eaf6bf09
Iris <ec101399@gmail.com>
2022-05-22 23:27:12 -0400

    wrote the whole thing, still need to source

--- ctfip.sh
+++ ctfip.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "hi"
+IDENTIFIER="ctfip script environment variable"
+TARGET="$HOME/.zshrc"
+sed -i "/$IDENTIFIER/c export ctfip=\"$1\" # $IDENTIFIER" $TARGET
+grep "$1" $TARGET -q && echo "successfully exported $1 to ctfip"
+#source $TARGET
